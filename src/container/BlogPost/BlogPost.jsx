c64cf01cd5236196429901d324d8c7578dc2ddb8
nailanabilaaaaaa <nailanabilaaaaaa@gmail.com>
2022-03-14 22:04:32 +0700

    Praktikum 1 - Pemindahan Statefull ke Stateless

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,20 +1,13 @@
 import React, {Component} from "react";
 import './BlogPost.css';
+import Post from "../../component/BlogPost/Post"
 
 class BlogPost extends Component{
     render() {
         return(
             <div class="post-artikel">
                 <h2>Daftar Artikel</h2>
-                <div class="artikel">
-                    <div class="gambar-artikel">
-                        <img src="http://placeimg.com/80/80/tech" alt="Gambar Tumbnail Artikel" />
-                    </div>
-                    <div class="konten-artikel">
-                        <div class="judul-artikel">Judul Artikel</div>
-                        <p class="isi-artikel">Isi Artikel</p>
-                    </div>
-                </div>
+                <Post/>
             </div>
         )
     }
