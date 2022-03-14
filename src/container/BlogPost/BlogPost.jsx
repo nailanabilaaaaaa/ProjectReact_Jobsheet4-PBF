373651670809c662bca48b765b4c797074cf3cf4
nailanabilaaaaaa <nailanabilaaaaaa@gmail.com>
2022-03-15 00:30:29 +0700

    Praktikum 2 - Modul 4

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -3,14 +3,32 @@ import './BlogPost.css';
 import Post from "../../component/BlogPost/Post"
 
 class BlogPost extends Component{
+    state = {
+        listArtikel: []
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3001/posts')
+            .then(response => response.json())
+            .then(result => {
+                this.setState({
+                    listArtikel: result
+                })
+            })
+    }
+
     render() {
-        return(
-            <div class="post-artikel">
+        return (
+            <div className="post-artikel">
                 <h2>Daftar Artikel</h2>
-                <Post judul="JTI Polinema" isi="Jurusan Teknologi Informasi - Politeknik Negeri Malang"/>
-            </div> 
+                {
+                    this.state.listArtikel.map(artikel => {
+                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body}/>
+                    })
+                }
+            </div>
         )
     }
 }
 
-export default BlogPost;
+export default BlogPost;  
